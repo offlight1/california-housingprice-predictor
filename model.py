a10b948c78f066097ed68422569916cce0eef7ef
offlight1 <kevinpuerto1010@gmail.com>
2025-03-21 19:42:52 -0400

    Improved accuracy
    Still a very inconistent and inaccurate model for the most part, but it has been improved. More work to be done.

--- model.py
+++ model.py
@@ -2,13 +2,19 @@ import tensorflow as tf
 import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
+from scipy.stats import zscore
+import numpy as np
 
 data = pd.read_csv("housing.csv")
 
-data.pop('ocean_proximity')
 data = data.dropna()
 
-features = data
+data['ocean_proximity'], _ = pd.factorize(data['ocean_proximity'])
+
+numeric_cols = data.select_dtypes(include=[np.number]).columns
+data = data[(np.abs(zscore(data[numeric_cols])) < 3).all(axis=1)]
+
+features = data.drop(['housing_median_age', 'total_bedrooms', 'population'], axis=1)
 labels = data['median_house_value']
 
 train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.2, random_state=42)
@@ -21,7 +27,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1, 'linear')
+    tf.keras.layers.Dense(1, activation='linear')
 ])
 
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
